15d6e890acbe516fd70711f562e120d2b301319e
Lateralny <piotrjgladysz@gmail.com>
2022-03-04 22:07:53 +0100

    update

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
 const { config } = require("dotenv");
 config();
 
-const COINBASE_API_KEY = '1234ae9d-0ca4-40f6-b863-8e9e1b1f3590';
+const COINBASE_API_KEY = process.env.COINBASE_API_KEY
+
 // process.env.COINBASE_API_KEY;
-const COINBASE_WEBHOOK_SECRET = 'e1a657b3-ba9f-4da4-9007-a4b4a0ac9feb';
+const COINBASE_WEBHOOK_SECRET = process.env.COINBASE_WEBHOOK_SECRET;
+
 //process.env.COINBASE_WEBHOOK_SECRET;
 const DOMAIN = 'http://localhost:3001';
 //process.env.DOMAIN;
